6b0c008397fc995350353bd780de4d25fd5dd63a
tonomolla6 <tonomollag6@gmail.com>
2022-08-02 09:32:26 +0200

    first commit

---
+++ Init.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * This file is part of PrintCheck plugin for FacturaScripts
+ * Copyright (C) 2022-2023 Tono Mollá González <mail@tonomolla.es>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+namespace FacturaScripts\Plugins\PrintCheck;
+
+use FacturaScripts\Core\Base\InitClass;
+
+/**
+ * Description of Init
+ *
+ * @author Tono Mollá González <mail@tonomolla.es>
+ */
+class Init extends InitClass
+{
+
+    public function init()
+    {
+        $this->loadExtension(new Extension\Controller\EditFacturaCliente());
+        $this->loadExtension(new Extension\Controller\EditAlbaranCliente());
+        $this->loadExtension(new Extension\Controller\ListFacturaCliente());
+        $this->loadExtension(new Extension\Controller\ListAlbaranCliente());
+    }
+
+    public function update() {
+    }
+}
